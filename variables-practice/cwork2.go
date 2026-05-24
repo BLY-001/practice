1e4268aa57908ef8a0b4179081db4bfcd0370b39
ybinlawa <yusufibnlawall@gmail.com>
2026-05-25 00:27:56 +0100

    emptyvariable

---
+++ variables-practice/cwork2.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main() {
+	var age int
+	var decimal float64
+	var name string
+	var flag bool
+	fmt.Println("first empty:", age)
+	fmt.Println("2nd empty:", decimal)
+	fmt.Println("3rd empty:", name)
+	fmt.Println("4th empty:", flag)
+}
