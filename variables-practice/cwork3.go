66283414013a929cbe46e83cbed981019d9d572b
ybinlawa <yusufibnlawall@gmail.com>
2026-05-25 00:35:23 +0100

    cwork3

---
+++ variables-practice/cwork3.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main() {
+	score := 10
+	fmt.Println("initial score:", score)
+	score = 20
+	fmt.Println("updated score:", score)
+	score = score + 30
+	fmt.Println("added score:", score)
+}
