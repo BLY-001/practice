a287485593ced89c1d3c32dcde7f29dbed56d2a2
ybinlawa <yusufibnlawall@gmail.com>
2026-05-25 08:14:23 +0100

    using constant and normal variable assignment

---
+++ variables-practice/varandconst.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+func main() {
+	const birthday = "15th of october" //cannot change and cannot be reassigned
+	var age = 25 // can change and can be reassigned
+	fmt.Println("my exact birthdate is:", birthday)
+	fmt.Println("my current age is :", age)
+
+}
