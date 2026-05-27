0fb57d43c405b216d9b66d45732bd8425314e6fd
ybinlawa <yusufibnlawall@gmail.com>
2026-05-27 22:48:34 +0100

    first classwork

---
+++ variables-practice/pinfocard.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+func main() {
+	name := "binlawal"
+	age := 26
+	height := 5.8
+	IsStudent := true
+
+	fmt.Println("my name is :", name)
+	fmt.Println("my age is :", age)
+	fmt.Printf("i am %.1fm tall\n", height)
+	fmt.Println("am i a student?", IsStudent)
+
+}
