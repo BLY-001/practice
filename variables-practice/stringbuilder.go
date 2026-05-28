c7fa5f4894ac6ff0d94e40825982f7599a09d817
ybinlawa <yusufibnlawall@gmail.com>
2026-05-28 21:07:34 +0100

    adding strings together

---
+++ variables-practice/stringbuilder.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+func main() {
+	firstname := "yusuf"
+	middlename := "Olatunji"
+	lastname := "Binlawal"
+	fullname := firstname + " " + middlename + " "+ lastname
+	fmt.Println("My fullname is :", fullname)
+}
