4dfeaeb34b49fc2f5fd1fc6d8f4a5d99bbeb3fd6
ybinlawa <yusufibnlawall@gmail.com>
2026-06-11 13:55:28 +0100

    using pointers to change value

--- pointers/chngtrupntr.go
+++ pointers/chngtrupntr.go
@@ -3,7 +3,7 @@ import "fmt"
 func main() {
 	balance := 500
 	fmt.Println("initial balance:", balance)
-	ptr := &balance
-	*ptr = 1000
+	ptr := &balance //ptr points to balance
+	*ptr = 1000 // change the value at that address to 1000
 	fmt.Println("new balance:", balance)
 }
