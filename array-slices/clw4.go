e142c2ca2165a9216cf4449195444a50c3a1fb4d
ybinlawa <yusufibnlawall@gmail.com>
2026-06-22 16:33:18 +0100

    create a new slice of weekdays and a new slice containing midweekdays

---
+++ array-slices/clw4.go
@@ -0,0 +1,8 @@
+package main
+import "fmt"
+func main() {
+	sl := []string{"sunday", "monday", "tuesday", "wednessday", "thursday", "friday", "saturday"}
+sl1 := sl[3:6]
+fmt.Println(sl1)
+fmt.Println(sl)
+}
